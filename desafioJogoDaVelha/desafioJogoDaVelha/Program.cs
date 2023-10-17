59b8887d034ba4b5c216eee5c06d117d4a47f942
Fernando Barros <fernandobarrosdesak@gmail.com>
2023-10-17 15:09:13 -0300

    adicionando o jogo da velha

---
+++ desafioJogoDaVelha/desafioJogoDaVelha/Program.cs
@@ -0,0 +1,115 @@
+﻿namespace desafioJogoDaVelha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool jogarNovamente = true;
+            while (jogarNovamente)
+            {
+                string[,] matriz = new string[3, 3];
+                string jogador = "X";
+                int index = 1;
+                string jogada;
+                int tentativas;
+
+                Console.WriteLine("JOGO DA VELHA");
+
+                Console.WriteLine();
+
+                for (int i = 0; i < matriz.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matriz.GetLength(1); j++)
+                    {
+                        matriz[i, j] = index.ToString();
+                        index++;
+                    }
+                }
+
+                for (tentativas = 1; tentativas < 9; tentativas++)
+                {
+                    for (int i = 0; i < matriz.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < matriz.GetLength(1); j++)
+                        {
+                            Console.Write("[" + matriz[i, j] + "]");
+                        }
+                        Console.WriteLine();
+                    }
+
+                    if ((matriz[0, 0] == matriz[1, 1] && matriz[1, 1] == matriz[2, 2]) ||
+                        (matriz[0, 2] == matriz[1, 1] && matriz[1, 1] == matriz[2, 0]) ||
+                        (matriz[0, 0] == matriz[0, 1] && matriz[0, 1] == matriz[0, 2]) ||
+                        (matriz[1, 0] == matriz[1, 1] && matriz[1, 1] == matriz[1, 2]) ||
+                        (matriz[2, 0] == matriz[2, 1] && matriz[2, 1] == matriz[2, 2]) ||
+                        (matriz[0, 0] == matriz[1, 0] && matriz[1, 0] == matriz[2, 0]) ||
+                        (matriz[0, 1] == matriz[1, 1] && matriz[1, 1] == matriz[2, 1]) ||
+                        (matriz[0, 2] == matriz[1, 2] && matriz[1, 2] == matriz[2, 2]))
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Fim de Jogo!!!");
+                        Console.WriteLine();
+                        Console.WriteLine("Parabéns!!! O ganhador é [" + jogador + "].");
+                        break;
+                    }
+
+                    if (jogador == "X")
+                    {
+                        jogador = "O";
+                    }
+                    else
+                    {
+                        jogador = "X";
+                    }
+
+                    Console.WriteLine();
+                    Console.WriteLine("Você quer jogar [" + jogador + "] em qual posição? ");
+                    jogada = Console.ReadLine();
+
+                    while (!"123456789".Contains(jogada))
+                    {
+                        Console.WriteLine();
+                        Console.Write("Jogada inválida. Tente Novamente: ");
+                        jogada = Console.ReadLine();
+                    }
+
+                    for (int i = 0; i < matriz.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < matriz.GetLength(1); j++)
+                        {
+                            if (matriz[i, j] == jogada)
+                            {
+                                matriz[i, j] = jogador;
+                            }
+                        }
+                    }
+
+                    Console.Clear();
+                }
+
+                if (tentativas == 9)
+                {
+                    for (int i = 0; i < matriz.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < matriz.GetLength(1); j++)
+                        {
+                            Console.Write("[" + matriz[i, j] + "]");
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("Fim de Jogo!!!");
+                    Console.WriteLine();
+                    Console.WriteLine("Que triste!!! Ninguém ganhou.");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Deseja jogar novamente? (s/n)");
+                char resposta = char.Parse(Console.ReadLine().ToUpper());
+                jogarNovamente = (resposta == 'S');
+
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ desafioJogoDaVelhaComputador/desafioJogoDaVelhaComputador/Program.cs
@@ -0,0 +1,211 @@
+﻿namespace DesafioJogoDaVelhaComputador;
+class Program
+{
+    static char[,] tabuleiro = new char[3, 3];
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("JOGO DA VELHA");
+        InicializarTabuleiro();
+        //DesenharTabuleiro();
+
+        while (true)
+        {
+            FazerJogadaEstrategicaComputador();
+            DesenharTabuleiro();
+
+            if (VerificarVitoria('O'))
+            {
+                Console.WriteLine("O computador ganhou!");
+                break;
+            }
+
+            if (VerificarEmpate())
+            {
+                Console.WriteLine("O jogo terminou em empate!");
+                break;
+            }
+
+            FazerJogadaHumano();
+            DesenharTabuleiro();
+
+            if (VerificarVitoria('X'))
+            {
+                Console.WriteLine("Parabéns! Você ganhou!");
+                break;
+            }
+
+            if (VerificarEmpate())
+            {
+                Console.WriteLine("O jogo terminou em empate!");
+                break;
+            }
+        }
+    }
+
+    static void InicializarTabuleiro()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                tabuleiro[i, j] = ' ';
+            }
+        }
+    }
+
+    static void DesenharTabuleiro()
+    {
+        Console.WriteLine("  0  1  2");
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write(i);
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write("[" + tabuleiro[i, j] + "]");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void FazerJogadaHumano()
+    {
+        Console.WriteLine("Sua vez. Digite a linha e a coluna separadas por espaço: ");
+        string[] input = Console.ReadLine().Split(' ');
+        int linha = int.Parse(input[0]);
+        int coluna = int.Parse(input[1]);
+
+        if (tabuleiro[linha, coluna] != ' ')
+        {
+            Console.WriteLine("Essa posição já está ocupada. Tente novamente.");
+            FazerJogadaHumano();
+        }
+        else
+        {
+            tabuleiro[linha, coluna] = 'X';
+        }
+    }
+
+    static void FazerJogadaEstrategicaComputador()
+    {
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (tabuleiro[i, j] == ' ')
+                {
+                    tabuleiro[i, j] = 'O';
+                    if (VerificarVitoria('O'))
+                    {
+                        return;
+                    }
+                    tabuleiro[i, j] = ' ';
+                }
+            }
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (tabuleiro[i, j] == ' ')
+                {
+                    tabuleiro[i, j] = 'X';
+                    if (VerificarVitoria('X'))
+                    {
+                        tabuleiro[i, j] = 'O';
+                        return;
+                    }
+                    tabuleiro[i, j] = ' ';
+                }
+            }
+        }
+
+        if (tabuleiro[1, 1] == ' ')
+        {
+            tabuleiro[1, 1] = 'O';
+            return;
+        }
+
+        if (tabuleiro[0, 0] == ' ')
+        {
+            tabuleiro[0, 0] = 'O';
+            return;
+        }
+
+        if (tabuleiro[0, 2] == ' ')
+        {
+            tabuleiro[0, 2] = 'O';
+            return;
+        }
+
+        if (tabuleiro[2, 0] == ' ')
+        {
+            tabuleiro[2, 0] = 'O';
+            return;
+        }
+
+        if (tabuleiro[2, 2] == ' ')
+        {
+            tabuleiro[2, 2] = 'O';
+            return;
+        }
+
+        FazerJogadaAleatoriaComputador();
+    }
+
+    static void FazerJogadaAleatoriaComputador()
+    {
+        Random random = new Random();
+        int linha, coluna;
+
+        do
+        {
+            linha = random.Next(0, 3);
+            coluna = random.Next(0, 3);
+        } while (tabuleiro[linha, coluna] != ' ');
+
+        tabuleiro[linha, coluna] = 'O';
+    }
+
+    static bool VerificarVitoria(char jogador)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            if (tabuleiro[i, 0] == jogador && tabuleiro[i, 1] == jogador && tabuleiro[i, 2] == jogador)
+            {
+                return true;
+            }
+
+            if (tabuleiro[0, i] == jogador && tabuleiro[1, i] == jogador && tabuleiro[2, i] == jogador)
+            {
+                return true;
+            }
+        }
+
+        if ((tabuleiro[0, 0] == jogador && tabuleiro[1, 1] == jogador && tabuleiro[2, 2] == jogador) ||
+            (tabuleiro[0, 2] == jogador && tabuleiro[1, 1] == jogador && tabuleiro[2, 0] == jogador))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    static bool VerificarEmpate()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (tabuleiro[i, j] == ' ')
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
